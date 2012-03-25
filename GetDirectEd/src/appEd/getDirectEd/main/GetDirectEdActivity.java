27568f3891133203c2a304605d9b770daffc2e5a
joshua <joshua@ua18.cs.ualberta.ca>
2012-03-25 13:34:21 -0600

    Yea

--- GetDirectEd/src/appEd/getDirectEd/main/GetDirectEdActivity.java
+++ GetDirectEd/src/appEd/getDirectEd/main/GetDirectEdActivity.java
@@ -1,19 +1,40 @@
 package appEd.getDirectEd.main;
 
-import android.app.Activity;
+
+
+import android.app.ListActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import android.widget.AdapterView.*;
 
-public class GetDirectEdActivity extends Activity {
+public class GetDirectEdActivity extends ListActivity {
 	
+	Resources res = getResources();
+	String[] choices = res.getStringArray(R.array.choices);
 	
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
+@Override
+public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
 
-        
-    }
+	// no more this
+	//setContentView(R.layout.list_fruit);
+
+	setListAdapter(new ArrayAdapter<String>(this, R.layout.main_view, choices));
+
+	ListView listView = getListView();
+	listView.setTextFilterEnabled(true);
+
+	listView.setOnItemClickListener(new OnItemClickListener() {
+		public void onItemClick(AdapterView<?> parent, View view,
+				int position, long id) {
+		    // When clicked, show a toast with the TextView text
+		    Toast.makeText(getApplicationContext(),
+			((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+		}
+	});
+
     
+}
 }
