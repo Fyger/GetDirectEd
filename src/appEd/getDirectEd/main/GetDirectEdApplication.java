c9e0bace5be7198e0570361f54172075dfbd2210
Fyger <jhelberg@ualberta.ca>
2012-04-02 11:18:34 -0600

    Added in the main application where in we can store our database adapters, as well as a class for the main text activity (start screen w/ text)

---
+++ src/appEd/getDirectEd/main/GetDirectEdApplication.java
@@ -0,0 +1,9 @@
+package appEd.getDirectEd.main;
+
+import android.app.Application;
+
+public class GetDirectEdApplication extends Application {
+	
+	
+
+}
---
+++ src/appEd/getDirectEd/views/MainTextActivity.java
@@ -0,0 +1,24 @@
+package appEd.getDirectEd.views;
+
+import android.app.Activity;
+import android.os.Bundle;
+import appEd.getDirectEd.main.R;
+
+public class MainTextActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_text);
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	
+	
+}
