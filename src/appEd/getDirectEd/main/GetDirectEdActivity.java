dc373a8433ee89514b234cae77b060a9a862474f
Fyger <jhelberg@ualberta.ca>
2012-04-09 14:17:24 -0600

    Final revisions

--- src/appEd/getDirectEd/main/GetDirectEdActivity.java
+++ src/appEd/getDirectEd/main/GetDirectEdActivity.java
@@ -1,34 +1,31 @@
 package appEd.getDirectEd.main;
 
-
-
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.*;
-
+/**
+ * @author James
+ * 
+ * the main activity, starts up and  
+ */
 public class GetDirectEdActivity extends Activity {
-	
-	
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-	
-		setContentView(R.layout.main_view);
-	
 		
-		Button button1 = (Button)findViewById(R.id.button1);
+		setContentView(R.layout.main_view);
+		
+		/*Button button1 = (Button)findViewById(R.id.facilityBtnMain);
 		button1.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
 				Intent intent = new Intent(GetDirectEdActivity.this, ActivitiesActivity.class);
             	startActivity(intent);       
 			}
-		});
+		});*/
 
 	}
 }
--- src/appEd/getDirectEd/main/GetDirectEdApplication.java
+++ src/appEd/getDirectEd/main/GetDirectEdApplication.java
@@ -3,7 +3,18 @@ package appEd.getDirectEd.main;
 import android.app.Application;
 
 public class GetDirectEdApplication extends Application {
-	
-	
 
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		//TODO: load in the settings for the program
+		//TODO: open some databases
+	}
+
+	@Override
+	public void onTerminate() {
+		super.onTerminate();
+		//TODO: close any open databases
+	}
+	
 }
